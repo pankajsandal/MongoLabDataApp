bdfdf761dbe539fcc9df3ddc059645e472d48d91
pankajsandal <pankajsandal1993@gmail.com>
2016-02-18 20:07:38 +0530

    added response header files to allow access for employee js

--- routes/Employee.js
+++ routes/Employee.js
@@ -2,6 +2,14 @@ var mongo = require("mongodb").MongoClient;
 var connString = require("../routes/ConnectionFile").connectionString;
 var express = require('express');
 var employeeRouter = express.Router();
+var bodyParser = require('body-parser');
+
+
+employeeRouter.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 employeeRouter.get('/',function(req,res)
 { 
@@ -61,9 +69,9 @@ employeeRouter.get('/:name',function(req,res)
 
 employeeRouter.post('/Insert/:name/:Dept/:Role',function(req,res)
 {
-    var name = req.param("name");
-    var Dept = req.param("Dept");
-    var role = req.param("Role");
+    var name = req.params.name;
+    var Dept = req.params.Dept;
+    var role = req.params.Role;
     
     mongo.connect(connString,function(dbErr,db){
        if(dbErr)
